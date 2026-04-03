b7a53616f122f8ac5a009e856e1c649ce2d0673c
Nawin chander <nawinnapzy289@gmail.com>
2026-04-03 13:00:12 +0530

    add

--- Test/Integration Testing.js
+++ Test/Integration Testing.js
@@ -4,5 +4,23 @@
 // Gateway correctly blocks requests
 // Middleware chaining
 
+const express = require("express");
+const app = express();
+
+const limiter = new TokenBucket(3, 1);
+
+app.use((req, res, next) => {
+  if (!limiter.allowRequest()) {
+    return res.status(429).send("Too Many Requests");
+  }
+  next();
+});
+
+app.get("/api", (req, res) => {
+  res.send("Success");
+});
+
+app.listen(3000);
+
 
 
