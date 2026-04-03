7b146aeaf9cf1dd4b03bbd5ef88e6661244eebab
Nawin chander <nawinnapzy289@gmail.com>
2026-04-03 13:03:56 +0530

    add

--- Test/Concurrency Testing.js
+++ Test/Concurrency Testing.js
@@ -5,5 +5,27 @@
 // Thread safety (even in Node async world)
 
 
+let counter = 0;
+
+async function fakeRequest() {
+  const temp = counter;
+  await new Promise(r => setTimeout(r, 10)); // simulate delay
+  counter = temp + 1;
+}
+
+async function testRace() {
+  await Promise.all(
+    Array.from({ length: 10 }, fakeRequest)
+  );
+
+  console.log(counter); // ❌ often < 10
+}
+
+testRace();
+
+
+/// fix atomic - like - logic
+
+
 
 
